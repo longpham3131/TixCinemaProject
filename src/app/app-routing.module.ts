ea294b64420f9d38d065878af69263c08108a3fc
longpham3131 <longpham3131@gmail.com>
2021-04-14 11:29:25 +0700

    create component sigin signup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthModule } from './auth/auth.module';
 import { MainModule } from './main/main.module';
 
-const routes: Routes = [{ path: '', loadChildren: () => MainModule }];
+const routes: Routes = [
+  { path: '', loadChildren: () => MainModule },
+  { path: '', loadChildren: () => AuthModule },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,9 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { AuthComponent } from './auth.component';
-
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
 @NgModule({
-  declarations: [AuthComponent],
-  imports: [CommonModule, AuthRoutingModule],
+  declarations: [AuthComponent, SigninComponent, SignupComponent],
+  imports: [
+    CommonModule,
+    AuthRoutingModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+  ],
 })
 export class AuthModule {}
